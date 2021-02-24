b78a02203487a562d0c7fdd3299b53d4b4cf146c
isaac silva <isaacsilvablues@outlook.com>
2021-02-24 15:55:45 -0300

    Finishing the timer setup and adding challenge box.

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,31 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+    const hasActiveChallenge = true;
+    return (
+        <div className={styles.challengeBoxContainer}>
+            { hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400 xp</header>
+                    <main>
+                        <img src="icons/body.svg" alt="Body icon"/>
+                        <strong>Novo desafio</strong>
+                        <p>Levante-se e alongue os braços e pernas.</p>
+                    </main>
+                    <footer>
+                        <button type="button" className={styles.challengeFailedButton}>Falhei</button>
+                        <button type="button" className={styles.challengeSucceededButton}>Completei</button>
+                    </footer>
+                </div>
+            ) : (
+                    <div className={styles.challengeNotActive}>
+                        <strong>Termine um ciclo para receber um novo desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="LevelUp photo" />
+                            Suba de nível completando desafios.
+                        </p>
+                    </div>
+                )}
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={ styles.container }>
+    <div className={styles.container}>
       <Head>
         <title>Inicio | Turn it off</title>
       </Head>
@@ -22,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
